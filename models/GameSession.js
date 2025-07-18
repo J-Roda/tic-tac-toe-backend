b0a3f1de0dda06d884788bca9f5c0d76d6d94407
Jayson Roda <jayson.roda96@gmail.com>
2025-07-18 17:59:29 +0800

    change chema structure

--- models/GameSession.js
+++ models/GameSession.js
@@ -2,20 +2,14 @@ const mongoose = require("mongoose");
 
 const GameSessionSchema = new mongoose.Schema(
     {
-        player1: String,
-        player2: String,
-        rounds: [
-            {
-                board: [[String]],
-                winner: String,
-            },
-        ],
+        player1: { type: String, required: true },
+        player2: { type: String, required: true },
+        roundsPlayed: Number,
         stats: {
             player1Wins: { type: Number, default: 0 },
             player2Wins: { type: Number, default: 0 },
             draws: { type: Number, default: 0 },
         },
-        isActive: { type: Boolean, default: true },
     },
     { timestamps: true }
 );
